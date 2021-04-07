cc649ec9d3bfff1a2ef49b2e17397130b27d2904
bien thanh danh <thanhdanh010499@gmail.com>
2021-04-07 14:51:05 +0700

    Add RegistAccount

---
+++ TMDT/Controllers/RegisterAccountController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT.Models;
+using TMDT.Function;
+
+namespace TMDT.Controllers
+{
+    public class RegisterAccountController : Controller
+    {
+        ChoDoCuEntities2 db = new ChoDoCuEntities2();
+        [HttpGet]
+        public ActionResult ThemNhanVien()
+        {
+            if (Session["idAccount"] == null) return RedirectToAction("DangNhap", "Login");
+            if (!Equals(Session["idRole"], 5)) return HttpNotFound();
+            DropDownListChucVu();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemNhanVien(FormCollection collection)
+        {
+            if (Session["idAccount"] == null) return RedirectToAction("DangNhap", "Login");
+            if (!Equals(Session["idRole"], 5)) return HttpNotFound();
+            DropDownListChucVu();
+            var hoten = collection["Fullname"];
+            var ngaysinh = String.Format("{0:dd/MM/yyy}", collection["Birthday"]);
+            var mail = collection["Email"];
+            var sdt = collection["Phonenumber"];
+            var mk = collection["Password"];
+            var mk1 = collection["Password1"];
+            var chucvu = collection["idRole"];
+            var dc = collection["Address"];
+
+            var csdt = from lg in db.Logins
+                       join acc in db.infoAccounts on lg.idAccount equals acc.idAccount
+                       where acc.PhoneNumber == sdt || lg.Email == mail
+                       select new { lg, acc };
+            foreach (var checksdt in csdt)
+            {
+                if (checksdt.acc.PhoneNumber == sdt)
+                {
+                    ViewData["Err1"] = "Số điện thoại này đã tồn tại trong hệ thống!";
+                    return View();
+                }
+                else if (checksdt.lg.Email == mail)
+                {
+                    ViewData["Err1"] = "Email này đã tồn tại trong hệ thống!";
+                    return View();
+                }
+                else return View();
+            }
+            if (String.IsNullOrEmpty(hoten))
+                ViewData["Err1"] = "Vui lòng nhập Họ tên!";
+            else if (String.IsNullOrEmpty(chucvu))
+                ViewData["Err1"] = "VUi lòng chọn Chức vụ!!";
+            else if (String.IsNullOrEmpty(ngaysinh))
+                ViewData["Err1"] = "Vui lòng nhập Ngày sinh!";
+            else if (String.IsNullOrEmpty(mail))
+                ViewData["Err1"] = "Vui lòng nhập Địa chỉ Email!";
+            else if (String.IsNullOrEmpty(sdt))
+                ViewData["Err1"] = "Vui lòng nhập Số điện thoại!";
+            else if (String.IsNullOrEmpty(dc))
+                ViewData["Err1"] = "Vui lòng nhập Địa chỉ!";
+            else if (String.IsNullOrEmpty(mk))
+                ViewData["Err1"] = "Vui lòng nhập Mật khẩu!";
+            else if (mk != mk1)
+                ViewData["Err1"] = "Mật khẩu nhập lại không đúng!";
+            else
+            {
+                infoAccount info = new infoAccount();
+                Login dn = new Login();
+
+                dn.Email = mail;
+                info.PhoneNumber = sdt;
+                dn.Password = _Function.md5(mk);
+                dn.hideAcc = false;
+                db.Logins.Add(dn);
+                db.SaveChanges();
+                var add = from lg in db.Logins
+                          where lg.Email == mail
+                          select lg.idAccount;
+                foreach (var c in add)
+                {
+                    info.idAccount = c;
+                    info.Fullname = hoten;
+                    info.Birthday = DateTime.Parse(ngaysinh);
+                    info.idRole = Int32.Parse(chucvu);
+                    info.Address = dc;
+                    db.infoAccounts.Add(info);
+                }
+                db.SaveChanges();
+                ViewData["Reg1"] = "Đăng ký thành công!";
+                return View();
+            }
+            return View();
+        }
+        // GET: RegisterAccount
+        
+    }
+}
