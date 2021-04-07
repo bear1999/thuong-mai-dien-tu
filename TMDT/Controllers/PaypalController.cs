331eb275e6ed95277cf86d61a07a148aac1c505a
quoctienzero4 <69066225+quoctienzero4@users.noreply.github.com>
2021-04-07 14:43:38 +0700

    Them trang Paypal

---
+++ TMDT/Controllers/PaypalController.cs
@@ -0,0 +1,94 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT.helper;
+using TMDT.Models;
+
+namespace TMDT.Controllers
+{
+    public class PaypalController : Controller
+    {
+        ChoDoCuEntities db = new ChoDoCuEntities();
+        private Payment payment;
+        // GET: Paypal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PaymentWithPaypal(string url)
+        {
+            if (Session["idAccount"] == null) return RedirectToAction("DangNhap", "Login");
+            //getting the apiContext as earlier
+            APIContext apiContext = helper.Configuration.GetAPIContext();
+            try
+            {
+                string payerId = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //this section will be executed first because PayerID doesn't exist
+                    //it is returned by the create function call of the payment class
+                    // Creating a payment
+                    // baseURL is the url on which paypal sendsback the data.
+                    // So we have provided URL of this controller only
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority + "/Paypal/PaymentWithPayPal?";
+                    //guid we are generating for storing the paymentID received in session
+                    //after calling the create function and it is used in the payment execution
+                    var guid = Convert.ToString((new Random()).Next(100000));
+                    //CreatePayment function gives us the payment approval url
+                    //on which payer is redirected for paypal account payment
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
+                    //get links returned from paypal in response to Create function  call
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be  redirected for payment
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid
+                    Session.Add(guid, createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This section is executed when we have received all the payments parameters
+                    // from the previous call to the function Create
+                    // Executing a payment
+                    var guid = Request.Params["guid"];
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("FailureView");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Error" + ex.Message);
+                return View("FailureView");
+            }
+            return ThanhToan();
+        }
+        public List<_GioHang> Laygiohang()
+        {
+            List<_GioHang> lstGiohang = Session["Giohang"] as List<_GioHang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<_GioHang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+            XoaTatcaGioHang();
+            return Content("<script language='javascript' type='text/javascript'>alert('Thanh toán thành công'); window.location.href='/trang-chu';</script></script>");
+        }
+    }
+}
